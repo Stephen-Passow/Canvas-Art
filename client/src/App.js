bf9308906953b5038c6e7138afb70bfc6229677e
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-11-25 13:44:08 -0500

    added react folder and made main 3 components for paintings/artists

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
