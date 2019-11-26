dc1c69c72b1a9b4c5339087885276d75e6f9fe51
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-11-25 19:30:14 -0500

    react installed and tested working

--- canvas_art_app/models.py
+++ canvas_art_app/models.py
@@ -26,4 +26,3 @@ class ArtistName(models.Model):
 
     def __str__(self):
         return self.name
-
--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import "./App.css"
+import axios from 'axios'
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <h1>Canvas Art!</h1>
-        <div><Link to ="/" /> </div>
+        <HomePage />
       </div>
     </Router>
   );
