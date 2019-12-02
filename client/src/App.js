87150641680ec3b937e227a7fa89b871d1e9fa6f
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-12-02 18:38:35 -0500

    removed color sorting components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css"
 import axios from 'axios'
 import HomePage from './components/HomePage';
--- client/src/components/BlackAndWhiteCategory.jsx
+++ client/src/components/BlackAndWhiteCategory.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+// import React, { Component } from 'react'
+// import axios from 'axios'
 
-export default class BlackAndWhiteCategory extends Component {
-    state = {
-        name: '',
-        description: '',
-        image: '',
-        category: ''
-    }
-    setCategory() {
-        this.state.category
-    }
-    componentDidMount() {
-        axios.get('/api/v1/painting/BlackAndWhite')
-    }
-    render() {
-        return (
-            <div>
+// export default class BlackAndWhiteCategory extends Component {
+//     state = {
+//         name: '',
+//         description: '',
+//         image: '',
+//         category: ''
+//     }
+//     setCategory() {
+//         this.state.category
+//     }
+//     componentDidMount() {
+//         axios.get('/api/v1/painting/')
+//     }
+//     render() {
+//         return (
+//             <div>
                 
-            </div>
-        )
-    }
-}
+//             </div>
+//         )
+//     }
+// }
